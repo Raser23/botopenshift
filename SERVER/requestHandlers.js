d9757ec8b5ff912f492525f823e43b9785565892
Neshchadim Serget <ser.neshadim@yandex.ru>
2016-01-10 18:10:51 +0600

    DEMO

--- SERVER/requestHandlers.js
+++ SERVER/requestHandlers.js
@@ -1,8 +1,10 @@
 var fs=require('fs');
 function main(res){
     fs.readFile('public/main.html',function(err,data){
-        if(err) throw err;
-
+        if(err) {
+            res.end(err.toString());
+            throw err;
+        }
         res.end(data);
     })
 }
