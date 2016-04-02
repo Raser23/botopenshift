68e34fb7da797c634f0fddcf84298238786c17da
Neshchadim Serget <ser.neshadim@yandex.ru>
2016-04-02 18:47:22 +0600

    opemshift

--- index.js
+++ index.js
@@ -62,21 +62,19 @@ function sendPhotoByBot(aChatId,photo){
     bot.sendPhoto(aChatId, photo, { caption: 'Image:' });
 }*/
 // An example for OpenShift platform.
+/
+// An example for OpenShift platform.
 var TelegramBot = require('node-telegram-bot-api');
 
 var token = '142893106:AAE9SE9xTcmXtS0QEOvzoMrrzcqAT-8H_HA';
 // See https://developers.openshift.com/en/node-js-environment-variables.html
-var port = process.env.PORT||3000;
-var host= 'botterino.herokuapp.com';
-//var host = "https://botterino.herokuapp.com/";
-var domain ="https://"+host+"/" + token;
-console.log(domain);
-var bot = new TelegramBot(token, {webHook: true});
-bot.getMe().then(function(me) {
-    console.log('Hello! My name is %s!', me.first_name);
+var port = process.env.OPENSHIFT_NODEJS_PORT;
+var host = process.env.OPENSHIFT_NODEJS_IP;
+var domain = process.env.OPENSHIFT_APP_DNS;
 
-});
-bot.setWebHook(host+"/"+token);
+var bot = new TelegramBot(token, {webHook: {port: port, host: host}});
+// OpenShift enroutes :443 request to OPENSHIFT_NODEJS_PORT
+bot.setWebHook(domain+':443/bot'+token);
 bot.on('message', function (msg) {
     var chatId = msg.chat.id;
     bot.sendMessage(chatId, "I'm alive!");
